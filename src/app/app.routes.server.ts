827d32105f6f60087cec1aaae155cd58191d3c52
Tijana Spirovska <spirovskagemini@gmail.com>
2025-05-27 12:45:33 +0200

    Success build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,18 +3,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   },
   {
     path: 'trip/:id',
     renderMode: RenderMode.Prerender,
     getPrerenderParams: async () => {
       // Example: Return a promise resolving to an array of IDs to prerender
-      return Promise.resolve([
-        { id: '1' },
-        { id: '2' },
-        { id: '3' }
-      ]);
-    }
-  }
+      return Promise.resolve([{ id: '1' }, { id: '2' }, { id: '3' }]);
+    },
+  },
 ];
